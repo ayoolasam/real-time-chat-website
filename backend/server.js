9862f0ca40a42200c0180614d65f8749175803c1
ayoolasam <obayomisamuel941@gmail.com>
2024-04-24 09:37:19 +0100

    updating

--- backend/server.js
+++ backend/server.js
@@ -2,13 +2,16 @@ const express = require('express')
 
 const chats= require('../backend/data/data') 
 const dotenv = require('dotenv')
-
+const mongoose = require('mongoose')
+const colors = require('colors')
 
 dotenv.config()
 const app = express()
 
 
-
+mongoose.connect('mongodb+srv://obayomisamuel941:Ayosam2403@cluster0.iuubs1l.mongodb.net/ayoola sam chat app').then(()=>{
+  console.log('database connected succesfully'.cyan)
+})
 
 
 
@@ -26,5 +29,5 @@ app.get('/api/chat/:id',(req,res) => {
 const PORT = process.env.PORT  || 5000
 
 app.listen(5000,()=>{
-  console.log(`Server running on port ${PORT}`)
+  console.log(`Server running on port ${PORT}`.cyan)
 })
